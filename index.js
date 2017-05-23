ed104368dfa3ad45e58466d611c5241c47d1490d
Gage Bachik <gbachik>
2017-05-23 01:00:12 -0700

    succ me t h o t

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ app.use('/', express.static(__dirname + '/'));
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 io.on('connection', function(socket){
-	console.log('BrowserSource Online')
+	console.log('RemoteSource Online')
+	socket.on('animation', function(num, inQueue, from){
+		console.log('animation', num, 'ran');
+		io.emit('animation', num, inQueue, from);
+	});
 });
 
 http.listen((process.env.PORT || 3200), function(){
-	console.log('listening on *:3002');
+	console.log('listening on env.Port');
 });
